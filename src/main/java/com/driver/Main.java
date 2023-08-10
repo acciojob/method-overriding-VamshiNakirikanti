af08c1335c0a7d285006bc0ee4e4d6619b1ea320
VamshiNakirikanri <vamshikrishna.nakirikanti@gmail.com>
2023-08-10 17:22:44 +0530

    Correcting files

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,20 @@
 package com.driver;
 
 public class Main {
-    B b = new B();
-    String s = b.meth();
+    public static class A{
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        public String meth() {
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+    }
 }
